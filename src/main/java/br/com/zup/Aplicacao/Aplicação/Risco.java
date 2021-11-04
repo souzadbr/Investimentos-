a7dbde967c3d69e678cba39244e43f755b68ef5c
Debora Souza <deboral.p@hotmail.com>
2021-11-04 11:34:25 -0300

    Modelar classe Enum

--- src/main/java/br/com/zup/Aplicacao/Aplicação/Risco.java
+++ src/main/java/br/com/zup/Aplicacao/Aplicação/Risco.java
@@ -1,8 +1,19 @@
 package br.com.zup.Aplicacao.Aplicação;
 
 public enum Risco {
-    ALTO,
-    MÉDIO,
-    BAIXO;
+
+    ALTO (0.25),
+    MÉDIO(0.15),
+    BAIXO(0.05);
+
+    private double taxa;
+
+    Risco (double taxa){
+        this.taxa = taxa;
+    }
+
+    public double getTaxa() {
+        return taxa;
+    }
 
 }
