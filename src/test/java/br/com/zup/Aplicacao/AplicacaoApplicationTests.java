bd0affca26c5cc3257e55be5062a465b40fd2864
Debora Souza <deboral.p@hotmail.com>
2021-11-04 10:58:14 -0300

    Commit inicial

---
+++ src/test/java/br/com/zup/Aplicacao/AplicacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.Aplicacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
