79b348fc1311658b0baeede830f7c8306d53c03d
Debora Souza <deboral.p@hotmail.com>
2021-11-04 11:14:03 -0300

    Modelar a classe investimento

---
+++ src/main/java/br/com/zup/Aplicacao/Aplicação/Investimento.java
@@ -0,0 +1,61 @@
+package br.com.zup.Aplicacao.Aplicação;
+
+public class Investimento {
+    private String email;
+    private String nome;
+    private String CPF;
+    private double valorPrevisto;
+    private int periodoDeAplicacaoMeses;
+    private Risco risco;
+
+    public Investimento() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public double getValorPrevisto() {
+        return valorPrevisto;
+    }
+
+    public void setValorPrevisto(double valorPrevisto) {
+        this.valorPrevisto = valorPrevisto;
+    }
+
+    public int getPeriodoDeAplicacaoMeses() {
+        return periodoDeAplicacaoMeses;
+    }
+
+    public void setPeriodoDeAplicacaoMeses(int periodoDeAplicacaoMeses) {
+        this.periodoDeAplicacaoMeses = periodoDeAplicacaoMeses;
+    }
+
+    public Risco getRisco() {
+        return risco;
+    }
+
+    public void setRisco(Risco risco) {
+        this.risco = risco;
+    }
+}
