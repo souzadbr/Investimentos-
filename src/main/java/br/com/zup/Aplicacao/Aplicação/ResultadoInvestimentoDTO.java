277a9325f592480f163b0f110fd550e208abcdb2
Debora Souza <deboral.p@hotmail.com>
2021-11-04 11:28:01 -0300

    Modelar classe ResultadoInvestimento

--- src/main/java/br/com/zup/Aplicacao/Aplicação/ResultadoInvestimentoDTO.java
+++ src/main/java/br/com/zup/Aplicacao/Aplicação/ResultadoInvestimentoDTO.java
@@ -1,4 +1,34 @@
 package br.com.zup.Aplicacao.Aplicação;
 
 public class ResultadoInvestimentoDTO {
+    private double valorInvestido;
+    private double valorTotalDoLucro;
+    private  double valorTotal;
+
+    public ResultadoInvestimentoDTO() {
+    }
+
+    public double getValorInvestido() {
+        return valorInvestido;
+    }
+
+    public void setValorInvestido(double valorInvestido) {
+        this.valorInvestido = valorInvestido;
+    }
+
+    public double getValorTotalDoLucro() {
+        return valorTotalDoLucro;
+    }
+
+    public void setValorTotalDoLucro(double valorTotalDoLucro) {
+        this.valorTotalDoLucro = valorTotalDoLucro;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
 }
